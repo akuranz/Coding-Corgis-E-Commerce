697285149e95934d84757e7a3ccd50533b4475a2
Abby Kuranz <akuranz@gmail.com>
2020-04-08 20:35:40 -0600

    merged AndDesign with service render

--- client/src/App.js
+++ client/src/App.js
@@ -16,90 +16,90 @@ import "./App.css";
 const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
-	const [state, setState] = useState({
-		loggedIn: false,
-		username: null
-	});
+  const [state, setState] = useState({
+    loggedIn: false,
+    username: null,
+  });
+  const [cart, setCart] = useState([]);
 
-	useEffect(() => {
-		axios.get("/auth").then(response => {
-			console.log("Get user response: ");
-			console.log(response.data);
-			if (response.data.user) {
-				console.log("Get User: There is a user saved in the server session: ");
-				setState(state => ({
-					...state,
-					loggedIn: true,
-					username: response.data.user.username
-				}));
-			} else {
-				console.log("Get user: no user");
-				setState(state => ({
-					...state,
-					loggedIn: false,
-					username: null
-				}));
-			}
-		});
-	}, []);
+  useEffect(() => {
+    axios.get("/auth").then((response) => {
+      console.log("Get user response: ");
+      console.log(response.data);
+      if (response.data.user) {
+        console.log("Get User: There is a user saved in the server session: ");
+        setState((state) => ({
+          ...state,
+          loggedIn: true,
+          username: response.data.user.username,
+        }));
+      } else {
+        console.log("Get user: no user");
+        setState((state) => ({
+          ...state,
+          loggedIn: false,
+          username: null,
+        }));
+      }
+    });
+  }, []);
 
-	const updateUser = () => {
-		setState({
-			...state
-		});
-	};
+  const updateUser = () => {
+    setState({
+      ...state,
+    });
+  };
 
-	return (
-		<Router>
-			<Layout>
-				<Sider
-					style={{ backgroundColor: "#443850" }}
-					breakpoint="lg"
-					collapsedWidth="0"
-					onBreakpoint={broken => {
-						console.log(broken);
-					}}
-					onCollapse={(collapsed, type) => {
-						console.log(collapsed, type);
-					}}
-				>
-					<SideMenu isLoggedIn={state.loggedIn} />
-				</Sider>
-				<Layout>
-					<Header
-						className="site-layout-sub-header-background"
-						style={{ padding: 0, backgroundColor: "#EAF0CE" }}
-					>
-						{/* <a id="headerLogo" href="/">
+  return (
+    <Router>
+      <Layout>
+        <Sider
+          style={{ backgroundColor: "#443850" }}
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <SideMenu isLoggedIn={state.loggedIn} />
+        </Sider>
+        <Layout>
+          <Header
+            className="site-layout-sub-header-background"
+            style={{ padding: 0, backgroundColor: "#EAF0CE" }}
+          >
+            {/* <a id="headerLogo" href="/">
 							<img src="../public/coding-corgi-logo-192h.png" alt="logo" />
 						</a> */}
-					</Header>
-					<Content style={{ margin: "24px 16px 0" }}>
-						<div
-							className="site-layout-background"
-							style={{ padding: 24, minHeight: 360 }}
-						>
-							<Route exact path="/" component={HomePage} />
-							<Route exact path="/loginSignup" component={loginSignup} />
-							<Route
-								exact
-								path="/Login"
-								render={() => <Login updateUser={updateUser} />}
-							/>
-							<Route exact path="/Signup" component={Signup} />
-							<Route exact path="/browse" component={Browse} />
-							<Route exact path="/cart" component={Cart} />
-							<Route exact path="/about" component={About} />
-						</div>
-					</Content>
-					<Footer style={{ textAlign: "center", backgroundColor: "#443850" }}>
-						<FooterContent />
-					</Footer>
-				</Layout>
-			</Layout>
-		</Router>
-	);
-
+          </Header>
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/loginSignup" component={loginSignup} />
+              <Route
+                exact
+                path="/Login"
+                render={() => <Login updateUser={updateUser} />}
+              />
+              <Route exact path="/Signup" component={Signup} />
+              <Route exact path="/browse" component={Browse} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/about" component={About} />
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center", backgroundColor: "#443850" }}>
+            <FooterContent />
+          </Footer>
+        </Layout>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -1,11 +1,27 @@
 import React from "react";
+import "antd/dist/antd.css";
+import { Card, Col, Row } from "antd";
 
 const Service = ({ service }) => {
   return (
-    <div>
-      <h1>
-        {service.language}: ${service.price}
-      </h1>
+    <div className="site-card-wrapper">
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="Card title" bordered={false}>
+            <ul>
+              <li>
+                <strong>Language:</strong> {service.language}
+              </li>
+              <li>
+                <strong>Price:</strong> {service.price}
+              </li>
+              <li>
+                <strong>Coder:</strong> {service.coder}
+              </li>
+            </ul>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
