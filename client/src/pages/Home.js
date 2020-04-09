6bb98d4f3c54afbc2f035c1490aa756cfb46d513
ambernina <rob.ambernina@gmail.com>
2020-04-08 19:36:48 -0600

    added top categories buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,43 @@
 import React from "react";
+import { Row, Col } from "antd";
 import HomeCarousel from "../components/HomeCarousel";
+import CatButton from "../components/CatButton";
 
-const Stuff = () => {
+const TopCats1 = [
+	"React",
+	"Javascript",
+	"HTML"
+];
+
+const TopCats2 = [
+	"Node.js",
+	"MySQL",
+	"MongoDB"
+];
+
+const HomePage = () => {
 	return (
 		<div>
 			<h1>Home</h1>
 			<HomeCarousel />
+			<div className="container" style={{ marginTop: 25 }}>
+					<h4>Top Categories</h4>
+				<Row>
+					<Col span={11}>
+						{TopCats1.map(cats => {
+							return <CatButton title={cats} />;
+						})}
+					</Col>
+					<Col span={2} />
+					<Col span={11}>
+						{TopCats2.map(cats => {
+							return <CatButton title={cats} />;
+						})}
+					</Col>
+				</Row>
+			</div>
 		</div>
 	);
 };
 
-export default Stuff;
+export default HomePage;
