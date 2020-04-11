108f605441497af99aa45fc3e36e1f7da355940d
Abby Kuranz <akuranz@gmail.com>
2020-04-11 15:05:54 -0600

    added reviews model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   Service: require("./Service"),
   User: require("./User"),
+  Review: require("./Review"),
 };
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-// This file empties the Todos collection and inserts the todos below
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/fullstackservices"
 );
@@ -13,72 +11,72 @@ const serviceSeed = [
     price: 75,
     selected: false,
     coder: "Amber",
-    reviews: [
-      {
-        review: "cool",
-        reviewer: "Abby",
-      },
-    ],
+    // reviews: [
+    //   {
+    //     review: "cool",
+    //     reviewer: "Abby",
+    //   },
+    // ],
   },
   {
     language: "HTML",
     price: 25,
     selected: false,
     coder: "Amber",
-    reviews: [
-      {
-        review: "cool",
-        reviewer: "Abby",
-      },
-    ],
+    // reviews: [
+    //   {
+    //     review: "cool",
+    //     reviewer: "Abby",
+    //   },
+    // ],
   },
   {
     language: "React.js",
     price: 150,
     selected: false,
     coder: "Amber",
-    reviews: [
-      {
-        review: "cool",
-        reviewer: "Abby",
-      },
-    ],
+    // reviews: [
+    //   {
+    //     review: "cool",
+    //     reviewer: "Abby",
+    //   },
+    // ],
   },
   {
     language: "Node.js",
     price: 125,
     selected: false,
     coder: "Amber",
-    reviews: [
-      {
-        review: "cool",
-        reviewer: "Abby",
-      },
-    ],
+    // reviews: [
+    //   {
+    //     review: "cool",
+    //     reviewer: "Abby",
+    //   },
+    // ],
   },
   {
     language: "MySQL",
     price: 75,
     selected: false,
     coder: "Amber",
-    reviews: [
-      {
-        review: "cool",
-        reviewer: "Abby",
-      },
-    ],
+    // reviews: [
+    //   {
+    //     review: "cool",
+    //     reviewer: "Abby",
+    //   },
+    // ],
   },
   {
     language: "MongoDB",
     price: 100,
     selected: false,
     coder: "Amber",
-    reviews: [
-      {
-        review: "cool",
-        reviewer: "Abby",
-      },
-    ],
+    // reviews: [
+    //   {
+    //     review: "cool",
+    //     reviewer: "Abby",
+    //   },
+    // ],
   },
 ];
 
@@ -87,10 +85,30 @@ const userSeed = {
   password: "1234",
 };
 
+const reviewSeed = [
+  {
+    review: "cool",
+    reviewer: "me",
+  },
+];
+
 const seedDB = async () => {
   try {
+    await db.Review.remove({});
     await db.Service.remove({});
     await db.User.remove({});
+    //connect reviews to services
+    const insertedR = await db.Review.collection.insertMany(reviewSeed);
+    const reviewIds = Object.values(insertedR.insertedIds);
+    const newService = await db.Service.create(serviceSeed);
+    const newServiceByID = newService.map((service) => {
+      return service.filter(id);
+    });
+    //new variable, loop over newService, filter by id of user
+    await db.User.findByIdAndUpdate(newService._id, {
+      $push: { review: { $each: reviewIds } },
+    });
+    //connect services to user
     const inserted = await db.Service.collection.insertMany(serviceSeed);
     const serviceIds = Object.values(inserted.insertedIds);
     const newUser = await db.User.create(userSeed);
@@ -99,7 +117,10 @@ const seedDB = async () => {
     });
 
     const demo = await db.User.findById(newUser._id).populate("services");
-    console.log(demo);
+    console.log("demo", demo);
+    console.log("newService", newService);
+    const demo2 = await db.User.findById(newServicebyId).populate("review");
+    console.log("demo2", demo2);
   } catch (error) {
     console.log(error);
   }
