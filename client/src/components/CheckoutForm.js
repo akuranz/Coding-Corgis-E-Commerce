b6e1de0b868f6a9888c2c19b81fbe81dea19dfbc
ambernina <rob.ambernina@gmail.com>
2020-04-11 13:35:56 -0600

    added checkout page and component

---
+++ client/src/components/CheckoutForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+// import { loadStripe } from "@stripe/stripe-js";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
+
+const CheckoutForm = () => {
+	const [product] = useState({
+    name: "Tesla Roadster",
+    price: 0.01,
+    description: "Cool car"
+  });
+
+  async function handleToken(token, addresses) {
+    console.log("token", token);
+    const response = await axios.post(
+      "/api/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>On Sale · ${product.price}</h3>
+      </div>
+      <StripeCheckout
+        stripeKey="pk_test_4TbuO6qAW2XPuce1Q6ywrGP200NrDZ2233"
+        token={handleToken}
+        amount={product.price * 100}
+        name="Tesla Roadster"
+        billingAddress
+        shippingAddress
+      />
+    </div>
+  );
+};
+
+export default CheckoutForm;
