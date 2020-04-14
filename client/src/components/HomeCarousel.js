dcd7d6bcda807210e6638f436ceb0e55e46d0374
ambernina <rob.ambernina@gmail.com>
2020-04-13 21:30:47 -0600

    added placeholder images

--- client/src/components/HomeCarousel.js
+++ client/src/components/HomeCarousel.js
@@ -1,20 +1,40 @@
 import React from "react";
-import { Carousel, Empty } from "antd";
+import { Carousel } from "antd";
 
 const HomeCarousel = () => {
 	return (
-		<Carousel autoplay style={{ backgroundColor: "#443850", height: 256 }}>
+		<Carousel
+			autoplay
+			style={{
+				backgroundColor: "#443850",
+				height: 260,
+				width: 775,
+				display: "flex"
+			}}
+		>
 			<div style={{ color: "#EAF0CE" }}>
-				<Empty style={{ padding: 50 }}/>
+				<img
+					src={require("../images/301-kim-Eydo2lQNfgU-unsplash.jpg")}
+					style={{ width: 775 }}
+				/>
 			</div>
 			<div style={{ color: "#EAF0CE" }}>
-				<Empty style={{ padding: 50 }} />
+				<img
+					src={require("../images/ruby-doan-iTkMgbiZ504-unsplash.jpg")}
+					style={{ width: 775 }}
+				/>
 			</div>
 			<div style={{ color: "#EAF0CE" }}>
-				<Empty style={{ padding: 50 }} />
+				<img
+					src={require("../images/bundo-kim-W6KdPX84whY-unsplash.jpg")}
+					style={{ width: 775 }}
+				/>
 			</div>
 			<div style={{ color: "#EAF0CE" }}>
-				<Empty style={{ padding: 50 }} />
+				<img
+					src={require("../images/helen-cheng-ty3FAU-hLUM-unsplash.jpg")}
+					style={{ width: 775 }}
+				/>
 			</div>
 		</Carousel>
 	);
