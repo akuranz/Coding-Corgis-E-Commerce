f3b7d5f8b682369dbd616bfe251a19f92164f518
ambernina <rob.ambernina@gmail.com>
2020-04-13 21:31:10 -0600

    added review text showing on card

--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -1,42 +1,43 @@
 import React from "react";
 import "antd/dist/antd.css";
-import { Card, Col, Button, Rate} from "antd";
+import { Card, Col, Button, Rate } from "antd";
 
 const Service = ({ service, selected, handleCart }) => {
-  return (
-    
-        <Col span={8}>
-          <Card bordered={false}>
-            <ul>
-              <li>
-                <strong>Language:</strong> {service.language}
-              </li>
-              <li>
-                <strong>Price:</strong> {service.price}
-              </li>
-              <li>
-                <strong>Coder:</strong> {service.coder}
-              </li>
-              
-            <Rate />
+  console.log("service components", service);
+	return (
+		<Col span={8}>
+			<Card bordered={false}>
+				<ul>
+					<li>
+						<strong>Language:</strong> {service.language}
+					</li>
+					<li>
+						<strong>Price:</strong> {service.price}
+					</li>
+					<li>
+						<strong>Coder:</strong> {service.coder}
+					</li>
+					<li>
+						<strong>Review:</strong> {service.review[0].review}
+					</li>
 
-              
-            </ul>
-            <Button
-              onClick={() => {
-                handleCart(service);
-              }}
-              data-id={service._id}
-            >
-              {selected ? "Remove" : "Add to Cart"}
-            </Button>
-          </Card>
-        </Col>
-  )
+					<Rate />
+				</ul>
+				<Button
+					onClick={() => {
+						handleCart(service);
+					}}
+					data-id={service._id}
+				>
+					{selected ? "Remove" : "Add to Cart"}
+				</Button>
+			</Card>
+		</Col>
+	);
 };
 
 Service.defaultProps = {
-  selected: false,
+	selected: false
 };
 
 export default Service;
