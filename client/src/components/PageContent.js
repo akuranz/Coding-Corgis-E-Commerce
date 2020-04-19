fa6b6aa29387348c272e5ab6c1e5bff0d2126259
ambernina <rob.ambernina@gmail.com>
2020-04-19 16:43:44 -0600

    added routes for individual browse components

--- client/src/components/PageContent.js
+++ client/src/components/PageContent.js
@@ -14,6 +14,18 @@ import Categories from "../pages/Categories";
 import Checkout from "../pages/Checkout";
 import AddAddress from "../pages/AddAddress";
 
+// browse components
+import BrowseReact from "./browse/BrowseReact";
+import BrowseJavascript from "./browse/BrowseJavascript";
+import BrowseHTML from "./browse/BrowseHTML";
+import BrowseNodejs from "./browse/BrowseNodejs";
+import BrowseMySQL from "./browse/BrowseMySQL";
+import BrowseMongoDB from "./browse/BrowseMongoDB";
+import BrowseCSS from "./browse/BrowseCSS";
+import BrowseSASS from "./browse/BrowseSASS";
+import BrowseRedux from "./browse/BrowseRedux";
+import BrowseAPIs from "./browse/BrowseAPIs";
+
 const { Content } = Layout;
 
 const PageContent = () => {
@@ -27,12 +39,22 @@ const PageContent = () => {
 				<Route exact path="/loginSignup" component={loginSignup} />
 				<Route exact path="/Login" component={Login} />
 				<Route exact path="/Signup" component={Signup} />
-				<Route exact path="/browse" component={Browse} />
 				<Route path="/cart" component={Cart} />
 				<Route exact path="/about" component={About} />
 				<Route exact path="/categories" component={Categories} />
 				<Route exact path="/checkout" component={Checkout} />
 				<Route exact path="/AddAddress" component={AddAddress} />
+				<Route exact path="/browse" component={Browse} />
+				<Route exact path="/browse/React" component={BrowseReact} />
+				<Route exact path="/browse/Javascript" component={BrowseJavascript} />
+				<Route exact path="/browse/HTML" component={BrowseHTML} />
+				<Route exact path="/browse/Node.js" component={BrowseNodejs} />
+				<Route exact path="/browse/MySQL" component={BrowseMySQL} />
+				<Route exact path="/browse/MongoDB" component={BrowseMongoDB} />
+				<Route exact path="/browse/CSS" component={BrowseCSS} />
+				<Route exact path="/browse/SASS" component={BrowseSASS} />
+				<Route exact path="/browse/Redux" component={BrowseRedux} />
+				<Route exact path="/browse/APIs" component={BrowseAPIs} />
 			</div>
 		</Content>
 	);
