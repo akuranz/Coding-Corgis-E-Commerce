bd6090b17f3b7afc28327c29bd8ddf5bd8c9c022
ambernina <rob.ambernina@gmail.com>
2020-04-20 20:27:59 -0600

    removed ignore build and added build

---
+++ client/build/precache-manifest.7c8f9fd5ed1d63ae54ffcc2ebd4fc2bb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cd9c7ac073f6b975c8636b9e236eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "000cdcd9dbc0eff5efd3",
+    "url": "/static/css/2.f9133ca1.chunk.css"
+  },
+  {
+    "revision": "c96c61156f8e630b0333",
+    "url": "/static/css/main.95428103.chunk.css"
+  },
+  {
+    "revision": "000cdcd9dbc0eff5efd3",
+    "url": "/static/js/2.0c51b251.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0c51b251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96c61156f8e630b0333",
+    "url": "/static/js/main.ad754742.chunk.js"
+  },
+  {
+    "revision": "32f04c269059affaff5e",
+    "url": "/static/js/runtime-main.e6a9157f.js"
+  },
+  {
+    "revision": "e586ae794e267aaabe6c233ccb73a591",
+    "url": "/static/media/chris-ried-ieic5Tq8YMk-unsplash.e586ae79.jpg"
+  },
+  {
+    "revision": "42608608544cd4e23a4ebc64642457b8",
+    "url": "/static/media/coding-corgi-logo-192h.42608608.png"
+  },
+  {
+    "revision": "fd04c1e1782bae802fd42cc201554ebe",
+    "url": "/static/media/dhaval-parmar-dnPniNPUe4o-unsplash.fd04c1e1.jpg"
+  },
+  {
+    "revision": "25f51426ef58b690eb3feb7e1406b434",
+    "url": "/static/media/ilya-pavlov-OqtafYT5kTw-unsplash.25f51426.jpg"
+  },
+  {
+    "revision": "d20a7909e920e27a2660ef7559b611f3",
+    "url": "/static/media/luca-bravo-XJXWbfSo2f0-unsplash.d20a7909.jpg"
+  }
+]);
